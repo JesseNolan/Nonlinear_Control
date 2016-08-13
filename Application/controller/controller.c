cf1f077cb18008ef2aceafb50dba1feaa1394df6
JesseNolan <jessenolan2@gmail.com>
2016-08-13 15:28:46 +1000

    Update controller.c

--- Application/controller/controller.c
+++ Application/controller/controller.c
@@ -1,24 +1,5 @@
 
 
-typedef struct{
-	long double a1;
-	long double a2;
-	long double a3;
-	long double a4;
-	long double a5;
-	long double b1;
-	long double b2;
-	long double b3;
-	long double b4;
-	long double b5;
-}	Con_coeffs;
-
-typedef struct{
-	long double e[5];
-	long double u[6];
-	long double y[2];
-} Signals;
-
 Con_coeffs con_coeffs;
 Signals signals;
 
